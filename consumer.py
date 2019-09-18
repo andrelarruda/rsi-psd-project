a7dfda62743fa3f9c5cfa7db433da63bb2793042
lucas sales <lucasg17.lima@gmail.com>
2019-09-18 10:13:07 -0300

    variaveis de sistema

--- consumer.py
+++ consumer.py
@@ -6,10 +6,11 @@ consumer = KafkaConsumer(
      auto_offset_reset='earliest',
      enable_auto_commit=True,
      group_id='my-group',
-     value_deserializer=lambda v: v.decode('utf-8'))
+     value_deserializer=lambda v: str(v).encode('utf-8'))
 
 consumer.subscribe(pattern=".+")
 
 for message in consumer:
+    print(message.topic)
     message = message.value
     print('received: ' + str(message))
--- gerador.py
+++ gerador.py
@@ -6,31 +6,39 @@ from datetime import datetime
 import pandas as pd
 import os
 import time
+import variaveis
+
+taxa = int(input("Digite a taxa de velocidade: "))
 
 producer = KafkaProducer(bootstrap_servers='localhost:9092', 
 value_serializer=lambda v: str(v).encode('utf-8'))
-arquivos = ["A301.csv", "A307.csv", "A309.csv", "A322.csv", "A328"]
-csv1_path = os.path.join(os.path.expanduser('~'), 'Desktop', 'projeto', 'data', 'A301.csv')
-#dataframe = pd.read_csv("F:/sample.csv", usecols=['id', 'First Name', 'Last Name', 'Email', 'Desciption', 'Role', 'Boss ID', 'Phone'], sep=',')
 
-#dataframe de empenhos
-dataframe = pd.read_csv(csv1_path,
-                        #Colunas que serão lidas
-                        usecols=['timestamp', 'stationCode', 'stationName', 'latitude', 'longitude', 'umid_max',
-                                    'umid_min', 'temp_max', 'pressao', 'pressao_min', 'pto_orvalho_inst',
-                                    'pto_orvalho_max', 'radiacao', 'temp_min', 'pressao_max', 'pto_orvalho_min',
-                                    'temp_inst', 'umid_inst', 'precipitacao'],
-                        sep=',',
-                        #Tipo das colunas
-                        dtype={'timestamp': int, 'stationCode': str, 'stationName': str, 'latitude': float, 'longitude': float, 'umid_max': int,
-                                    'umid-min': int, 'temp_max': float, 'pressao': float, 'pressao_min': float, 'pto_orvalho_inst': float,
-                                    'pto_orvalho_max': float, 'radiacao': float, 'temp_min': float, 'pressao_max': float, 'pto_orvalho_min': float,
-                                    'temp_inst': float, 'umid_inst': int, 'precipitacao': float})
-taxa = int(input("Digite a taxa de velocidade: "))
-for element in dataframe.values:
-    mensagem = str(element[17]) + " " + str(element[16])
-    producer.send(element[1] +".humidade.temperatura", mensagem )
-    time.sleep(1 / taxa)
+arquivos = ["A301.csv", "A307.csv", "A309.csv", "A322.csv", "A328.csv", "A329.csv", "A341.csv", "A349.csv", "A350.csv", 
+"A351.csv", "A357.csv", "A366.csv", "A370.csv"]
+
+for file in arquivos:
+    csv1_path = os.path.join(os.path.expanduser('~'), variaveis.var1, variaveis.var2, variaveis.var3, file)
+    #dataframe = pd.read_csv("F:/sample.csv", usecols=['id', 'First Name', 'Last Name', 'Email', 'Desciption', 'Role', 'Boss ID', 'Phone'], sep=',')
+
+    #dataframe de empenhos
+    dataframe = pd.read_csv(csv1_path,
+                            #Colunas que serão lidas
+                            usecols=['timestamp', 'stationCode', 'stationName', 'latitude', 'longitude', 'umid_max',
+                                        'umid_min', 'temp_max', 'pressao', 'pressao_min', 'pto_orvalho_inst',
+                                        'pto_orvalho_max', 'radiacao', 'temp_min', 'pressao_max', 'pto_orvalho_min',
+                                        'temp_inst', 'umid_inst', 'precipitacao'],
+                            sep=',',
+                            #Tipo das colunas
+                            dtype={'timestamp': int, 'stationCode': str, 'stationName': str, 'latitude': float, 'longitude': float, 'umid_max': int,
+                                        'umid-min': int, 'temp_max': float, 'pressao': float, 'pressao_min': float, 'pto_orvalho_inst': float,
+                                        'pto_orvalho_max': float, 'radiacao': float, 'temp_min': float, 'pressao_max': float, 'pto_orvalho_min': float,
+                                        'temp_inst': float, 'umid_inst': int, 'precipitacao': float})
+
+    for element in dataframe.values:
+        mensagem = str(element[17]) + " " + str(element[16])
+        print(mensagem)
+        producer.send(element[1] +".humidade.temperatura", mensagem )
+        time.sleep(1 / taxa)
 
 
 #    dataframe.to_sql('intermediaria_empenho', conn, if_exists='replace')
