be4cb21719d405c5d270d35ccfce4b4e39c385a2
misaelta <misaelt.a@hotmail.com>
2019-10-12 20:19:23 -0300

    beta layout teste

--- app/App.js
+++ app/App.js
@@ -1,10 +1,20 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TouchableOpacity,TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>HelloWorld</Text>
+    <View  style={styles.container}>
+      <Text style={styles.h1Text}>Calculador de IH</Text>
+      <View style={styles.inputBox}>
+      <TextInput style={styles.input} placeholder="Longitude" placeholderTextColor="black"></TextInput>
+      <TextInput style={styles.input} placeholder="Latitude" placeholderTextColor="black"></TextInput>
+      
+      </View>
+      <TouchableOpacity style={styles.button}><Text style={styles.textButton}>Calcular</Text></TouchableOpacity>
+      <Text style={styles.txtResult}>Resultado: 10 </Text>
+
+
+
     </View>
   );
 }
@@ -14,6 +24,46 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent:"center"
   },
+  inputBox:{
+      marginTop:10,
+     flexDirection:"row",
+     height:200,
+     backgroundColor:"#fff"
+     
+  },
+  input:{
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: '#d6d7da',
+    height:50,
+    textAlign:"center",
+    width:"50%",
+    fontSize:15,
+    marginTop:50,
+    backgroundColor:"gray"
+  },
+  h1Text:{
+    fontSize:50
+  },
+  txtResult:{
+    fontSize:20,
+    marginTop:70
+  },
+  textButton:{
+    fontSize:15,
+    
+  },
+  button:{
+    width:"50%",
+    alignItems:"center",
+    justifyContent:"center",
+    backgroundColor:"blue",
+    borderRadius:5,
+    borderWidth:2,
+    height:50,
+    marginBottom:100
+    
+  }
 });
