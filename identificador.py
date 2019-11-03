c0e54509634fb07fe6265c405cc4e70f134619fb
lucas sales <lucasg17.lima@gmail.com>
2019-11-03 16:01:05 -0300

    finalizacao de identificador

--- identificador.py
+++ identificador.py
@@ -6,7 +6,6 @@ class Devices():
         self._broker        = "localhost"
         self._port          = 1883
         self._clients       = []
-        self._connect_flag  = False
         self._tokens        = {'A301': "f4bCXGwj9Mk6cArVwJSc", 'A307': "ngC1wVtcAS6eRDxjmLjF", 
         'A309': "7W00vXj4nqYvzhrB1y3J", 'A322': "au7bVNpWPgho0jEEQSZ5", 'A328': "AWTccpmlqqvtsuDcC9ma", 
         'A329': "6VYmn1TgkIurtYwf6BTm", 'A341': "rZJ3TWbrt3iOgkThdRpA", 'A349': "yk64ImmTFJGCR5vNXdVH", 
@@ -26,9 +25,8 @@ class Devices():
         for client in self._clients:
             current_client = str(client._client_id, "UTF8")
             if current_client == code["values"]["stationCode"]:
-                client.loop_start()
-                ret = client.publish(thingsboard, payload, retain= True)
-                time.sleep(1)
+                #client.loop_start()
+                ret = client.publish(thingsboard, str(payload), retain= True)
                 print(ret)
                 break
         
@@ -40,7 +38,5 @@ class Devices():
             client.connect(self._broker,self._port, keepalive=60)
             client.on_publish = self.on_publish
             client.username_pw_set(self._tokens[i])
-
+            client.loop_start()
             self._clients.append(client)
-
-        self._connect_flag = True
