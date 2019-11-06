f8505bb6dbb23bf3170772b01a04c96ad96edfba
misael <misaelta.contato@gmail.com>
2019-11-06 16:55:35 -0300

    cidades

--- api/server.js
+++ api/server.js
@@ -46,19 +46,41 @@ const cities={
     lat:-8.598785,
     long:-38.584062
   },
-  
-
-
-
-
-
-
-
+  Palmares:{
+    lat:-8.666667,
+    long:-35.567921
+  },
+  Ouricuri:{
+    lat:-7.885833,
+    long:-40.102683
+  },
+  Salgueiro:{
+    lat:-8.0580556,
+    long:-39.096111
+  },
 }
 
 
-console.log(cities.Recife)
 
+
+function getDistance(lat1,lat2,long1,long2){
+
+
+const raioTerra=6371
+
+let dLat = (lat2-lat1) //diferença das latitudes dos pontos em radianos
+let dLon = (long2-long1) //diferença das longitudes dos pontos em radianos
+let a = Math.sin(dLat/2) * Math.sin(dLat/2) + Math.cos(lat1) * Math.cos(lat2) * Math.sin(dLon/2) * Math.sin(dLon/2)
+let c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a)); 
+let distancia = raioTerra * c;
+
+return distancia;
+
+}
+
+let resposta= getDistance(-7.9970741,-8.014858,-34.927654,-34.949444)
+
+console.log(resposta)
 
 app.get('/:lat/:long',(req,res)=>{
 
