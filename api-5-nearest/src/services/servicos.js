70a6b297e69f1c84a9a8546dff1474f5e47f9886
Andre Arruda <andre.luizarruda@ufrpe.br>
2019-11-13 16:47:30 -0300

    Funções básicas da api criadas.

---
+++ api-5-nearest/src/services/servicos.js
@@ -0,0 +1,48 @@
+function getDistance(lat1, lon1, lat2, lon2) {
+	Number.prototype.toRad = function () {
+		return this * Math.PI / 180;
+	}
+	var R = 6371;
+	var x1 = lat2 - lat1;
+	var dLat = x1.toRad();
+	var x2 = lon2 - lon1;
+	var dLon = x2.toRad();
+	var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+		Math.cos(lat1.toRad()) * Math.cos(lat2.toRad()) *
+		Math.sin(dLon / 2) * Math.sin(dLon / 2);
+	var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+	var d = R * c;
+
+	return d
+
+}
+
+function getCidadeByName(listaCidades, nome){
+	let result;
+	for (let i = 0 ; i < listaCidades.length ; i++){
+		if (listaCidades[i].nome == nome){
+			result = listaCidades[i];
+			break;
+		}
+	}
+	return result;
+}
+
+function getCidadeMenorDistancia(lat, lon, cidades) {
+	var cidadeMaisProxima
+	let distancia = 1000000000;
+
+
+	for (cidade in cidades) {
+		let resultado = getDistance(cidades[cidade].lat, cidades[cidade].long, lat, lon)
+		if (resultado < distancia) {
+			distancia = resultado
+			cidadeMaisProxima = cidade
+
+		}
+
+	}
+	return cidadeMaisProxima
+}
+
+module.exports = {getCidadeMenorDistancia,getDistance}
