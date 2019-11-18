84e3474d35ecc875ed988164cc7047797a99294a
Andre Arruda <andre.luizarruda@ufrpe.br>
2019-11-18 18:42:31 -0300

    Requisição automática do token.

--- api-5-nearest/src/routes.js
+++ api-5-nearest/src/routes.js
@@ -2,9 +2,8 @@ const routes = require('express').Router();
 const data = require('./db/data.json');
 const servicos = require('./services/servicos');
 const axios = require('axios').default;
-const getToken = require('./services/getToken');
 const instance = axios.create({
-	baseURL: 'http://172.16.206.40:9090'
+	baseURL: 'http://localhost:9090'
 });
 let TOKEN = null;
 
@@ -26,39 +25,38 @@ routes.get('/api/5nearest/:lat/:long', (req, res) => {
 
 
 routes.get('/teste', async (req, res) => {
+
+	//setar o id do Device
+	const deviceID = "67f61240-0a2c-11ea-a698-0bc6fc91ecdf"
+
 	// FAZ REQUISIÇÃO DO TOKEN - OK
-	// const requisicaoToken = await axios({
-	// 	method: 'post',
-	// 	url: 'http://localhost:9090/api/auth/login',
-	// 	data: { 
-	// 		username: "tenant@thingsboard.org", password: "tenant"
-	// 	},	
-	// })
+	const requisicaoToken = await axios({
+		method: 'post',
+		url: 'http://localhost:9090/api/auth/login',
+		data: {
+			username: "tenant@thingsboard.org", password: "tenant"
+		},
+	})
 
-	TOKEN = getToken;
+	TOKEN = requisicaoToken.data.token;
 
-	console.log(TOKEN);
+	xAuthorization = 'Bearer ' + TOKEN;
 
-	return res.json()
+	instance.defaults.headers.common['X-Authorization'] = xAuthorization;
+	instance.get(`/api/plugins/telemetry/DEVICE/${deviceID}/values/timeseries?keys=HI,umidade,temperatura`)
+		.then(response => {
+			// console.log(response)
+			return res.status(200).json(response.data);
+		})
+		.catch(err => {
+			//console.log(err);
+			return res.status(401).json(err)
+		});
+
 
 	// http://localhost:9090/swagger-ui.html#!/telemetry-controller/getTimeseriesKeysUsingGET
 	// https://thingsboard.io/docs/reference/rest-api/
 	// https://thingsboard.io/docs/user-guide/telemetry/#data-query-api
-	
-
-
-
-
-	// instance.defaults.headers.common['X-Authorization'] = 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJ0ZW5hbnRAdGhpbmdzYm9hcmQub3JnIiwic2NvcGVzIjpbIlRFTkFOVF9BRE1JTiJdLCJ1c2VySWQiOiJkNzlmOGYxMC1jZDhhLTExZTktYTAzNC0xNWFlYzhhZjRmMGQiLCJlbmFibGVkIjp0cnVlLCJpc1B1YmxpYyI6ZmFsc2UsInRlbmFudElkIjoiZDc1NWRiZTAtY2Q4YS0xMWU5LWEwMzQtMTVhZWM4YWY0ZjBkIiwiY3VzdG9tZXJJZCI6IjEzODE0MDAwLTFkZDItMTFiMi04MDgwLTgwODA4MDgwODA4MCIsImlzcyI6InRoaW5nc2JvYXJkLmlvIiwiaWF0IjoxNTc0MDg1NDA2LCJleHAiOjE1NzQwOTQ0MDZ9.uKoT3XVAXRsuqHWd3gkoZTbiUaXOsAWAU0-FUWacFFAOyry62AyRomFsSScIJbT9ER-yzU9AUmmrODMxYujtjg'
-	// instance.get('/api/plugins/telemetry/DEVICE/75434b80-e5fa-11e9-bebd-e35bcf2d23e2/values/timeseries?keys=HI,umidade,temperatura')
-	// 	.then(response => {
-	// 		console.log(response)
-	// 		return res.status(200).json(response.data);
-	// 	})
-	// 	.catch(err => {
-	// 		//console.log(err);
-	// 		return res.status(401).json(err)
-	// 	});
 });
 
 module.exports = routes;
