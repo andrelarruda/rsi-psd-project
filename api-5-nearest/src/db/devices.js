9754f0015f49e8a35d9736c8072ce8b0b7d318df
lucas sales <lucasg17.lima@gmail.com>
2019-11-20 09:31:52 -0300

    ids

--- api-5-nearest/src/db/devices.js
+++ api-5-nearest/src/db/devices.js
@@ -2,16 +2,16 @@
 
 module.exports = {
 	Recife:"67f61240-0a2c-11ea-a698-0bc6fc91ecdf",
-	Petrolina:"",
-	Arcoverde:"",
-	Garanhuns:"",
-	Surubim:"",
-	Cabrobo:"",
-	Caruaru:"",
-	Ibimirim:"",
-	"Serra Talhada":"",
-	Floresta:"",
-	Palmares:"77f00580-0a58-11ea-bd97-470d24539586",
-	Ouricuri:"",
-	Salgueiro:""
+	Petrolina:"89a04ec0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Arcoverde:"91dc9d50-e5fa-11e9-bebd-e35bcf2d23e2",
+	Garanhuns:"a3177900-e5fa-11e9-bebd-e35bcf2d23e2",
+	Surubim:"b4ccb4d0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Cabrobo:"c47f69e0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Caruaru:"d37d4de0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Ibimirim:"e790ec10-e5fa-11e9-bebd-e35bcf2d23e2",
+	Serra_Talhada:"f56718f0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Floresta:"ffe986f0-e5fa-11e9-bebd-e35bcf2d23e2",
+	Palmares:"0c9f4fb0-e5fb-11e9-bebd-e35bcf2d23e2",
+	Ouricuri:"16c08ae0-e5fb-11e9-bebd-e35bcf2d23e2",
+	Salgueiro:"2130f780-e5fb-11e9-bebd-e35bcf2d23e2"
 }
